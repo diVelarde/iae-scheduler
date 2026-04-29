bc2301acaef2d3e49da4b744fe19acf7e624948a
diVelarde <divelarde@gbox.adnu.edu.ph>
2026-04-29 13:04:18 +0800

    Configure database to Prisma

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const pool = require("./db");
+const pool = require("./config/db");
 const scheduleRoutes = require("./routes/scheduleRoutes");
 
 const app = express();
